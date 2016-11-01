a539d6fe1c0ecb2a371e0439b0d9deadddb55630
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-10-31 22:17:47 -1000

    Initial Commit - basic point and click coding

---
+++ Assets/Scripts/TextFaceCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFaceCamera : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+}
